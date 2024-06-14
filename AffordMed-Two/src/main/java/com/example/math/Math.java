82b6f7b57b53fded7b79eca5ecb96a723a82bf11
GURU CHARAN R <119277923+GuruCharan27@users.noreply.github.com>
2024-06-14 12:57:29 +0530

    Add files via upload

---
+++ AffordMed-Two/src/main/java/com/example/math/Math.java
@@ -0,0 +1,14 @@
+package com.example.math;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("http://localhost:9876/numbers")
+public class Math {
+	
+	@GetMapping("/e")
+	public 
+
+}
